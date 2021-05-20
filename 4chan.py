8fbbab2e571782b91a34bfd335312bb0934dfade
Nick <Devilmanaltair1995@yahoo.com>
2021-05-20 05:06:41 +0300

    Update 4chan.py

--- 4chan.py
+++ 4chan.py
@@ -7,10 +7,10 @@ from bs4 import BeautifulSoup
 import re
 import os
 
+forb=('<>:"/\|?*')
 fpage=input("Input thread to start downloading all media: ")
 url=fpage
 r = requests.get(url)
-
 names=list()
 links=list()
 soup = BeautifulSoup(requests.get(url).text, 'lxml')
@@ -18,9 +18,12 @@ containers = soup.findAll("div", {"class": "fileText"})
 left='href="//'
 right='" target'
 
+#Folder name
 dirname = soup.find("title").text
 print(dirname)
 dirname=dirname[dirname.find('-')+2:dirname.rfind('-')-1]
+for char in forb:
+    dirname = dirname.replace(char,'!')
 if not os.path.exists(dirname):
     os.makedirs(dirname)
 os.chdir(dirname)
@@ -28,11 +31,10 @@ os.chdir(dirname)
 for index,c in enumerate(containers):
     s = (str(c))
     n = (str(c.text))
-    
     links.append(s[s.index(left)+len(left):s.index(right)])
     names.append(c.find('a', href=True).text)
-    print ('HTML:',s,'\nLINK: ',s[s.index(left)+len(left):s.index(right)],'\nFull',c.text,'\nCLEAN NAME: ', (n[n.index('File: ')+len('File: '):n.index(' (')]),'\nINDEX: ',index,'\nPercentage Completed:',round((index+1)*100/len(containers)),'\n')
+    print ('HTML:',s,'\nLINK: ',s[s.index(left)+len(left):s.index(right)],'\nFull',c.text,'\nCLEAN NAME:', (n[n.index('File: ')+len('File: '):n.index(' (')]),'\nINDEX: ',index,'\nPercentage Completed:',round((index+1)*100/len(containers)),'\n')
     k = requests.get('http://'+links[-1], allow_redirects=True)
     open(names[-1], 'wb').write(k.content)
     
-    #rule of three for percentage
+    #Rule of three for percentage
